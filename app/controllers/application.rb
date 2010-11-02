5f2d0f97e0120aabe337db6c676814c72f18e52f
Philippe Robert <softserv@sbcglobal.net>
2010-11-02 08:46:58 -0500

    Iteration D1: Moving the Cart

--- app/controllers/application.rb
+++ app/controllers/application.rb
@@ -2,9 +2,12 @@
 # Likewise, all the methods added will be available for all controllers.
 
 class ApplicationController < ActionController::Base
+
+  session :session_key => '_depot_session_id'
+
   helper :all # include all helpers, all the time
 
   # See ActionController::RequestForgeryProtection for details
   # Uncomment the :secret if you're not using the cookie session store
-  protect_from_forgery # :secret => '00a5e06e1ca7e514e4a7764d5784d4af'
+  protect_from_forgery  :secret => '00a5e06e1ca7e514e4a7764d5784d4af'
 end
--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,8 +1,65 @@
 class StoreController < ApplicationController
 
+before_filter :find_cart, :except => :empty_cart
+
 def index
-@time = Time.now
-@products = Product.find_products_for_sale
+  @time = Time.now
+  @products = Product.find_products_for_sale
 end
+
+
+# def add_to_cart
+#   @cart = find_cart
+#  product = Product.find(params[:id])
+#   @cart.add_product(product)
+# end
+
+  def add_to_cart
+
+    product = Product.find(params[:id])
+
+    @cart = find_cart
+
+    @cart.add_product(product)
+    
+    redirect_to_index("Added product")
+
+  rescue ActiveRecord::RecordNotFound
+
+    logger.error("Attempt to access invalid product #{params[:id]}")
+
+    redirect_to_index("Invalid product")
+
+  end
+
+
+def empty_cart
+  session[:cart] = nil
+  flash[:notice] = "Your cart is currently empty"
+  redirect_to :action => :index
+end
+
+
+
+private
+
+
+def find_cart
+  @cart = (session[:cart] ||= Cart.new)
+end
+
+
+def redirect_to_index(msg)
+  flash[:notice] = msg
+  redirect_to :action => :index
+end
+
+
+protected
+
+  def authorize
+  end
+
+
 
 end
